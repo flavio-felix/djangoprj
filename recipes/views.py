17b7382c62bacb3df480f626390b237eb901fccd
flavio-felix <desenhista.felix@yahoo.com.br>
2025-01-15 18:54:48 -0300

    preparacao das headers, search inputs

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name':'Flavio Felix'})
-
-def contato(request):
-    return render(request, 'deletar/temp.html')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
+    return render(request, 'recipes/pages/home.html')
